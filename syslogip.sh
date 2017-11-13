097f23391e36231796792af2948c0f6ee7a0fd40
Unknown <lexh94@hotmail.com>
2017-11-13 15:44:26 +0100

    new stuff

--- syslogip.sh
+++ syslogip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Reads IP address needed to receive syslog logs.
+#Reads IP address needed to receive syslog logs. NOTE: Enter the address of the RECEIVING host.
 echo $'\nEnter IP address:\n'
 read ipaddrng
 echo $'\n'
-sed -i 's/#source s_net { tcp(ip(127.0.0.1) port(1000)); };/source s_network { syslog(ip($ipaddrng) transport("tcp")); };/g' /etc/syslog-ng/syslog-ng.conf
+sed -i 's/syslog(ip(10.0.0.0)/syslog(ip($ipaddrng)/g' /etc/syslog-ng/syslog-ng.conf
