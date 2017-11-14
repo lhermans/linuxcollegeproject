97f732c62fbd66f0fc2d5182381030e6c8692fe2
Unknown <lexh94@hotmail.com>
2017-11-14 11:14:59 +0100

    Initial salt master script

---
+++ saltmaster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Install salt related packages
+apt-get install salt-api
+apt-get install salt-cloud
+apt-get install salt-master
+apt-get install salt-minion
+apt-get install salt-ssh
+apt-get install salt-syndic
+
